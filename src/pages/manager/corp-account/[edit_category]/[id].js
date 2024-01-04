071aa7955ea75cdcd485ecf757a396331508995e
rlawndks4 <rlawndks0423@naver.com>
2024-01-04 17:56:56 +0900

    update

---
+++ src/pages/manager/corp-account/[edit_category]/[id].js
@@ -0,0 +1,246 @@
+
+import { Button, Card, FormControl, Grid, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { themeObj } from "src/components/elements/styled-components";
+import { useSettingsContext } from "src/components/settings";
+import { Upload } from "src/components/upload";
+import ManagerLayout from "src/layouts/manager/ManagerLayout";
+import { toast } from "react-hot-toast";
+import { useModal } from "src/components/dialog/ModalProvider";
+import dynamic from "next/dynamic";
+import { apiManager, apiServer } from "src/utils/api-manager";
+import { bankCodeList } from "src/utils/format";
+import { useAuthContext } from "src/auth/useAuthContext";
+import _ from "lodash";
+const ReactQuill = dynamic(() => import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+})
+
+const VirtualAccountEdit = () => {
+  const { setModal } = useModal()
+  const { user } = useAuthContext();
+  const { themeMode, themeDnsData } = useSettingsContext();
+
+  const router = useRouter();
+
+  const [mchtList, setMchtList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [item, setItem] = useState({
+    mid: '',
+    deposit_bank_code: '',
+    deposit_acct_num: '',
+    deposit_acct_name: '',
+    birth: '',
+    phone_num: '',
+    type: 0,
+  })
+
+  useEffect(() => {
+    settingPage();
+  }, [])
+  const settingPage = async () => {
+    let mcht_list = await apiManager(`users`, 'list', {
+      level: 10,
+    })
+    let data = item;
+    setMchtList(mcht_list?.content ?? []);
+    let mid = _.find(mcht_list?.content, { id: parseInt(user?.id) })?.mid;
+    data.mid = mid;
+    if (router.query?.edit_category == 'edit') {
+      data = await apiManager('virtual-accounts', 'get', {
+        id: router.query.id
+      })
+    }
+    setItem(data);
+
+    setLoading(false);
+  }
+  const onSave = async () => {
+    let result = undefined
+    result = await apiServer(`${process.env.API_URL}/api/acct/v1/issuance`, 'create', { ...item, api_key: themeDnsData?.api_key, });
+    if (result?.tid) {
+      toast.success("성공적으로 저장 되었습니다.");
+      router.push('/manager/virtual-account');
+    }
+  }
+  const oneWonCertification = async () => {
+    let result = await apiServer(`${process.env.API_URL}/api/acct/v1`, 'create', {
+      mid: item?.mid,
+      bank_code: item?.deposit_bank_code,
+      account: item?.deposit_acct_num,
+      name: item?.deposit_acct_name,
+      birth: item?.birth,
+      phone_num: item?.phone_num,
+      guid: item?.guid,
+      api_key: themeDnsData?.api_key,
+      user_id: user?.id
+    });
+    let data = item;
+    data.guid = result?.guid;
+    if (result?.tid) {
+      toast.success('성공적으로 발송 되었습니다.');
+      data = {
+        ...data,
+        is_send_one_won_check: true,
+        tid: result?.tid,
+      }
+    }
+    setItem(data);
+  }
+  const checkOneWonCertification = async () => {
+    let result = await apiServer(`${process.env.API_URL}/api/acct/v1/check`, 'create', {
+      mid: item?.mid,
+      tid: item?.tid,
+      vrf_word: item?.vrf_word,
+      guid: item?.guid,
+      api_key: themeDnsData?.api_key,
+    });
+    if (result?.tid) {
+      toast.success('성공적으로 인증 되었습니다.');
+      setItem({
+        ...item,
+        is_check_bank: true
+      })
+    }
+  }
+  return (
+    <>
+      {!loading &&
+        <>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={12}>
+              <Card sx={{ p: 2, height: '100%' }}>
+                <Stack spacing={3}>
+                  {user?.level >= 40 &&
+                    <>
+                      <FormControl variant='outlined'  >
+                        <InputLabel>가맹점선택</InputLabel>
+                        <Select label='가맹점선택' value={item?.mid}
+                          onChange={(e) => {
+                            setItem({
+                              ...item,
+                              mid: e.target.value,
+                            })
+                          }}>
+                          {mchtList.map(mcht => {
+                            return <MenuItem value={mcht?.mid}>{`${mcht?.nickname}(${mcht?.user_name})`}</MenuItem>
+                          })}
+                        </Select>
+                      </FormControl>
+                    </>}
+                  <TextField
+                    label='MID'
+                    value={item.mid}
+                    disabled={true}
+                  />
+                  <TextField
+                    label='생년월일'
+                    value={item.birth}
+                    placeholder="19990101"
+                    onChange={(e) => {
+                      setItem(
+                        {
+                          ...item,
+                          ['birth']: e.target.value
+                        }
+                      )
+                    }} />
+                  <TextField
+                    label='휴대폰번호'
+                    value={item.phone_num}
+                    placeholder="하이픈(-) 제외 입력"
+                    onChange={(e) => {
+                      setItem(
+                        {
+                          ...item,
+                          ['phone_num']: e.target.value
+                        }
+                      )
+                    }} />
+                  <Stack spacing={1}>
+                    <FormControl>
+                      <InputLabel>입금은행</InputLabel>
+                      <Select
+                        label='입금은행'
+                        value={item.deposit_bank_code}
+                        onChange={e => {
+                          setItem({
+                            ...item,
+                            ['deposit_bank_code']: e.target.value
+                          })
+                        }}
+                      >
+                        {bankCodeList().map((itm, idx) => {
+                          return <MenuItem value={itm.value}>{itm.label}</MenuItem>
+                        })}
+                      </Select>
+                    </FormControl>
+                  </Stack>
+                  <TextField
+                    label='입금계좌번호'
+                    value={item.deposit_acct_num}
+                    onChange={(e) => {
+                      setItem(
+                        {
+                          ...item,
+                          ['deposit_acct_num']: e.target.value
+                        }
+                      )
+                    }} />
+                  <TextField
+                    label='입금자명'
+                    value={item.deposit_acct_name}
+                    onChange={(e) => {
+                      setItem(
+                        {
+                          ...item,
+                          ['deposit_acct_name']: e.target.value
+                        }
+                      )
+                    }} />
+                  <Button onClick={oneWonCertification} variant="outlined" style={{ height: '48px', }}>1원인증 발송</Button>
+                  {item.is_send_one_won_check &&
+                    <>
+                      <TextField
+                        label='인증번호'
+                        value={item.vrf_word}
+                        placeholder=""
+                        onChange={(e) => {
+                          setItem(
+                            {
+                              ...item,
+                              ['vrf_word']: e.target.value
+                            }
+                          )
+                        }} />
+                      <Button disabled={item?.is_check_bank} onClick={checkOneWonCertification} variant="outlined" style={{ height: '48px', }}>{item?.is_check_bank ? '확인완료' : '인증확인'}</Button>
+                    </>}
+                </Stack>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={12}>
+              <Card sx={{ p: 3 }}>
+                <Stack spacing={1} style={{ display: 'flex' }}>
+                  <Button variant="contained" style={{
+                    height: '48px', width: '120px', marginLeft: 'auto'
+                  }} onClick={() => {
+                    setModal({
+                      func: () => { onSave() },
+                      icon: 'material-symbols:edit-outline',
+                      title: '저장 하시겠습니까?'
+                    })
+                  }}>
+                    저장
+                  </Button>
+                </Stack>
+              </Card>
+            </Grid>
+          </Grid>
+        </>}
+    </>
+  )
+}
+VirtualAccountEdit.getLayout = (page) => <ManagerLayout>{page}</ManagerLayout>;
+export default VirtualAccountEdit
---
+++ src/pages/manager/corp-account/list.js
@@ -0,0 +1,249 @@
+import { Avatar, Button, Card, CardContent, Chip, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, FormControl, IconButton, InputLabel, MenuItem, Select, Stack, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import ManagerTable from "src/views/manager/table/ManagerTable";
+import { Icon } from "@iconify/react";
+import { useRouter } from "next/router";
+import { Col, Row } from "src/components/elements/styled-components";
+import { toast } from "react-hot-toast";
+import { useModal } from "src/components/dialog/ModalProvider";
+import ManagerLayout from "src/layouts/manager/ManagerLayout";
+import { apiManager } from "src/utils/api-manager";
+import { getUserLevelByNumber } from "src/utils/function";
+import { useAuthContext } from "src/auth/useAuthContext";
+import _ from "lodash";
+import { bankCodeList, operatorLevelList, virtualAccountStatusList } from "src/utils/format";
+import { useSettingsContext } from "src/components/settings";
+
+const VirtualAccountList = () => {
+  const { setModal } = useModal()
+  const { user } = useAuthContext();
+  const { themeDnsData } = useSettingsContext();
+  const defaultColumns = [
+    {
+      id: 'user_name',
+      label: '가맹점',
+      action: (row, is_excel) => {
+        if (is_excel) {
+          return `${row[`nickname`]} (${row['user_name']})`
+        }
+        if (row['user_name']) {
+          return <div style={{ whiteSpace: 'pre' }}>{`${row[`nickname`]}\n(${row['user_name']})`}</div>
+        } else {
+          return "---";
+        }
+      }
+    },
+    {
+      id: 'virtual_bank_code',
+      label: '가상계좌은행',
+      action: (row, is_excel) => {
+        return _.find(bankCodeList(), { value: row['virtual_bank_code'] })?.label ?? "---"
+      }
+    },
+    {
+      id: 'virtual_acct_num',
+      label: '가상계좌번호',
+      action: (row, is_excel) => {
+        return row['virtual_acct_num'] ?? "---"
+      }
+    },
+    {
+      id: 'virtual_acct_name',
+      label: '가상계좌명',
+      action: (row, is_excel) => {
+        return row['virtual_acct_name'] ?? "---"
+      }
+    },
+    {
+      id: 'guid',
+      label: 'USER GUID',
+      action: (row, is_excel) => {
+        return row['guid'] ?? "---"
+      }
+    },
+    {
+      id: 'status',
+      label: '상태',
+      action: (row, is_excel) => {
+        if (is_excel) {
+          return _.find(virtualAccountStatusList, { value: row?.status })?.label
+        }
+        return <Chip variant="soft" label={_.find(virtualAccountStatusList, { value: row?.status })?.label} color={_.find(virtualAccountStatusList, { value: row?.status })?.color} />
+      }
+    },
+    {
+      id: 'deposit_bank_code',
+      label: '입금은행정보',
+      action: (row, is_excel) => {
+        if (is_excel) {
+          return `${_.find(bankCodeList(), { value: row['deposit_bank_code'] })?.label ?? "---"} ${row['deposit_acct_num']} ${row['deposit_acct_name']}`
+        }
+        return <Col>
+          <div>{_.find(bankCodeList(), { value: row['deposit_bank_code'] })?.label ?? "---"}</div>
+          <div>{row['deposit_acct_num']} {row['deposit_acct_name']}</div>
+        </Col>
+      }
+    },
+
+    {
+      id: 'created_at',
+      label: '생성일',
+      action: (row, is_excel) => {
+        return row['created_at'] ?? "---"
+      }
+    },
+    {
+      id: 'edit',
+      label: '이어서 생성',
+      action: (row, is_excel) => {
+        if (is_excel) {
+          return `---`
+        }
+        return row['status'] == 5 ? <>
+          <IconButton>
+            <Icon icon='material-symbols:edit-outline' onClick={() => {
+              router.push(`edit/${row?.id}`)
+            }} />
+          </IconButton>
+        </> : "---"
+      }
+    },
+    ...(user?.level >= 40 ? [
+      {
+        id: 'delete',
+        label: '삭제',
+        action: (row, is_excel) => {
+          if (is_excel) {
+            return `---`
+          }
+          if (row?.is_delete == 1) {
+            return "---";
+          }
+          return (
+            <>
+              <IconButton onClick={() => {
+                setModal({
+                  func: () => { deleteUser(row?.id) },
+                  icon: 'material-symbols:delete-outline',
+                  title: '정말 삭제하시겠습니까?'
+                })
+              }}>
+                <Icon icon='material-symbols:delete-outline' />
+              </IconButton>
+            </>
+          )
+        }
+      },
+    ] : []),
+  ]
+  const router = useRouter();
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState({});
+  const [operUserList, setOperUserList] = useState([]);
+  const [searchObj, setSearchObj] = useState({
+    page: 1,
+    page_size: 10,
+    s_dt: '',
+    e_dt: '',
+    search: '',
+    is_sales_man: true,
+  })
+  const [dialogObj, setDialogObj] = useState({
+    changePassword: false,
+  })
+  const [changePasswordObj, setChangePasswordObj] = useState({
+    id: '',
+    user_pw: ''
+  })
+  useEffect(() => {
+    pageSetting();
+
+  }, [])
+  const pageSetting = () => {
+    let cols = defaultColumns;
+    setColumns(cols)
+    getAllOperUser();
+    onChangePage({ ...searchObj, page: 1, });
+  }
+  const getAllOperUser = async () => {
+    let data = await apiManager('users', 'list', {
+      level: 10,
+    });
+    setOperUserList(data?.content ?? []);
+  }
+  const onChangePage = async (obj) => {
+    setData({
+      ...data,
+      content: undefined
+    })
+    let data_ = await apiManager('virtual-accounts', 'list', obj);
+    if (data_) {
+      setData(data_);
+    }
+    setSearchObj(obj);
+  }
+  const deleteUser = async (id) => {
+    let data = await apiManager('virtual-accounts', 'delete', { id });
+    if (data) {
+      onChangePage(searchObj);
+    }
+  }
+
+  return (
+    <>
+      <Stack spacing={3}>
+        <Card>
+          <Row style={{ padding: '12px', columnGap: '0.5rem', flexWrap: 'wrap', rowGap: '0.5rem' }}>
+            <FormControl variant='outlined' size='small' sx={{ minWidth: '150px' }}>
+              <InputLabel>가맹점</InputLabel>
+              <Select label='가맹점' value={searchObj[`mcht_id`]}
+                onChange={(e) => {
+                  onChangePage({ ...searchObj, [`mcht_id`]: e.target.value })
+                }}>
+                <MenuItem value={null}>가맹점 전체</MenuItem>
+                {operUserList.filter(el => el?.level == 10).map(oper => {
+                  return <MenuItem value={oper?.id}>{`${oper?.nickname}(${oper?.user_name})`}</MenuItem>
+                })}
+              </Select>
+            </FormControl>
+            <FormControl variant='outlined' size='small' sx={{ minWidth: '150px' }}>
+              <InputLabel>상태</InputLabel>
+              <Select label='상태' value={searchObj[`status`]}
+                onChange={(e) => {
+                  onChangePage({ ...searchObj, [`status`]: e.target.value })
+                }}>
+                <MenuItem value={null}>상태 전체</MenuItem>
+                {virtualAccountStatusList.map(status => {
+                  return <MenuItem value={status?.value}>{status.label}</MenuItem>
+                })}
+              </Select>
+            </FormControl>
+            <FormControl variant='outlined' size='small' sx={{ minWidth: '150px' }}>
+              <InputLabel>삭제여부</InputLabel>
+              <Select label='삭제여부' value={searchObj[`is_delete`]}
+                onChange={(e) => {
+                  onChangePage({ ...searchObj, [`is_delete`]: e.target.value })
+                }}>
+                <MenuItem value={0}>삭제안됨</MenuItem>
+                <MenuItem value={1}>삭제됨</MenuItem>
+              </Select>
+            </FormControl>
+          </Row>
+
+          <ManagerTable
+            data={data}
+            columns={columns}
+            searchObj={searchObj}
+            onChangePage={onChangePage}
+            add_button_text={'가상계좌 발급'}
+            head_columns={[]}
+            table={'virtual-accounts'}
+            excel_name={'가상계좌'}
+          />
+        </Card>
+      </Stack>
+    </>
+  )
+}
+VirtualAccountList.getLayout = (page) => <ManagerLayout>{page}</ManagerLayout>;
+export default VirtualAccountList
