9aa207721e0a7b5d1efade5b8ac2cc9b7a32175e
rlawndks4 <rlawndks0423@naver.com>
2024-02-23 15:45:26 +0900

    update

--- src/views/api/deposit/v1.js
+++ src/views/api/deposit/v1.js
@@ -1,32 +1,11 @@
-import { Button, Card, FormControl, Grid, InputLabel, MenuItem, Select, Stack, TextField, Typography, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
-import { useState } from "react";
-import { Row, themeObj, Col } from "src/components/elements/styled-components";
-import { useSettingsContext } from "src/components/settings";
-import styled from "styled-components";
+import { getLocalStorage } from "src/utils/local-storage";
 
-export const Title2 = styled.h2`
-margin: 1rem auto;
-font-size:${themeObj.font_size.size3};
-font-weight:bold;
-@media (max-width:1000px) {
-  font-size:${themeObj.font_size.size5};
-}
-`
-
-export const Title3 = styled.h3`
-margin: 1rem auto 1rem 0;
-font-size:${themeObj.font_size.size6};
-font-weight:bold;
-white-space: pre;
-@media (max-width:1000px) {
-  font-size:${themeObj.font_size.size7};
-}
-`
 
 const DepositApiV1 = () => {
 
-    const { themeDnsData } = useSettingsContext();
-    const [currentTab, setCurrentTab] = useState(0);
+    let themeDnsData = getLocalStorage('themeDnsData');
+    themeDnsData = JSON.parse(themeDnsData);
+
     const tab_list = [
         {
             value: 0,
@@ -154,111 +133,11 @@ const DepositApiV1 = () => {
 
         },
     }
-    const returnTable = (table_head = [], table_body = []) => {
-        if (table_head.length > 0 && table_body.length > 0) {
-            return <Table style={{ border: '1px solid #ccc' }}>
-                <TableHead>
-                    <TableRow sx={{ padding: '1rem 0' }}>
-                        {table_head.map(text => (
-                            <>
-                                <TableCell style={{ textAlign: 'center' }}>
-                                    {text}
-                                </TableCell>
-                            </>
-                        ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {table_body.map(col => (
-                        <>
-                            <TableRow sx={{ padding: '1rem 0' }}>
-                                {col && col.map(row => (
-                                    <>
-                                        <TableCell style={{ textAlign: 'center' }}>
-                                            {row}
-                                        </TableCell>
-                                    </>
-                                ))}
-                            </TableRow>
-                        </>
-                    ))}
-                </TableBody>
-            </Table>
-        }
+    return {
+        tab_list,
+        table_obj,
+        tab_key: 'depositv1'
     }
-    return (
-        <>
-            <Row style={{ margin: '0 0 1rem 0', columnGap: '0.5rem' }}>
-                {tab_list.map((tab) => (
-                    <Button
-                        variant={tab.value == currentTab ? 'contained' : 'outlined'}
-                        onClick={() => {
-                            setCurrentTab(tab.value)
-                        }}
-                    >{tab.label}</Button>
-                ))}
-            </Row>
-            <Grid container spacing={3}>
-                <Grid item xs={12} md={12}>
-                    <Card sx={{ p: 2, height: '100%' }}>
-                        <Stack spacing={3}>
-                            {tab_list.map((itm, idx) => (
-                                <>
-                                    {currentTab == itm.value &&
-                                        <>
-                                            <Title3 style={{ fontWeight: 'normal', color: '#777' }}>
-                                                {table_obj[itm.value].explain}
-                                            </Title3>
-                                            {table_obj[itm.value].uri &&
-                                                <>
-                                                    <Title3>
-                                                        [ Request ]
-                                                    </Title3>
-                                                    <Col style={{ padding: '2rem', background: '#222', rowGap: '0.5rem' }}>
-                                                        <Row style={{ columnGap: '0.5rem' }}>
-                                                            <div style={{ color: '#fff' }}>POST</div>
-                                                            <div style={{ color: 'yellow' }}>{process.env.API_URL}{table_obj[itm.value].uri}</div>
-                                                            <div style={{ color: '#fff' }}>HTTP/1.1</div>
-                                                        </Row>
-                                                        <Row style={{ columnGap: '0.5rem' }}>
-                                                            <div style={{ color: '#fff' }}>Host:</div>
-                                                            <div style={{ color: 'orange' }}>{process.env.API_URL}</div>
-                                                        </Row>
-                                                        <Row style={{ columnGap: '0.5rem' }}>
-                                                            <div style={{ color: '#fff' }}>Service Port:</div>
-                                                            <div style={{ color: 'orange' }}>443</div>
-                                                        </Row>
-                                                    </Col>
-                                                </>}
-                                            {table_obj[itm.value].req_head?.length > 0 && table_obj[itm.value].req_body?.length > 0 &&
-                                                <>
-                                                    <Title3 style={{ fontWeight: 'normal', color: '#777' }}>
-                                                        요청 바디는 JSON 객체로 구성됩니다.
-                                                    </Title3>
-                                                    {returnTable(table_obj[itm.value].req_head, table_obj[itm.value].req_body,)}
-                                                </>}
-                                            <Title3>
-                                                [ Response ]
-                                            </Title3>
-                                            <Title3 style={{ fontWeight: 'normal', color: '#777' }}>
-                                                응답 바디는 JSON 객체로 구성됩니다.
-                                            </Title3>
-                                            {returnTable(table_obj[itm.value].res_head, table_obj[itm.value].res_body,)}
-                                            {table_obj[itm.value]?.res_head && table_obj[itm.value]?.data_res_body &&
-                                                <>
-                                                    <Title3 style={{ fontWeight: 'normal', color: '#777' }}>
-                                                        data 구성요소 입니다.
-                                                    </Title3>
-                                                </>}
-                                            {returnTable(table_obj[itm.value].res_head, table_obj[itm.value].data_res_body,)}
-                                        </>}
-                                </>
-                            ))}
-                        </Stack>
-                    </Card>
-                </Grid>
-            </Grid>
-        </>
-    )
+
 }
 export default DepositApiV1;
